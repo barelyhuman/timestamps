70669c1eae7583595a87b9c1c3b13d6022a18d1a
Reaper Gelera <ahoy@barelyhuman.dev>
2023-05-01 13:59:27 +0530

    feat: add parser

--- src/index.mjs
+++ src/index.mjs
@@ -46,3 +46,71 @@ export function formatMilliseconds(mills, pattern) {
   const inSeconds = mills / 1000
   return formatSeconds(inSeconds, pattern)
 }
+
+/**
+ * @param {number} mills
+ * @param {string} pattern
+ */
+export function parse(timestamp, pattern = '{hh}h {mm}m {ss}s') {
+  const output = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  }
+
+  const patternSequence = []
+
+  const regex = pattern
+
+    // Sanitize Pattern
+    .replace('(', '\\(')
+    .replace(')', '\\)')
+    .replace('[', '\\[')
+    .replace(']', '\\]')
+    .replace('|', '\\|')
+
+    // Construct Regex and find match positions
+    .replace('{hh}', (_, offset) => {
+      patternSequence[offset] = 'hours_long'
+      return '(\\d{2})'
+    })
+    .replace('{h}', (_, offset) => {
+      patternSequence[offset] = 'hours_short'
+      return '(\\d{1})'
+    })
+    .replace('{mm}', (_, offset) => {
+      patternSequence[offset] = 'mins_long'
+      return '(\\d{2})'
+    })
+    .replace('{m}', (_, offset) => {
+      patternSequence[offset] = 'mins_short'
+      return '(\\d{1})'
+    })
+    .replace('{ss}', (_, offset) => {
+      patternSequence[offset] = 'seconds_long'
+      return '(\\d{2})'
+    })
+    .replace('{s}', (_, offset) => {
+      patternSequence[offset] = 'seconds_short'
+      return '(\\d{1})'
+    })
+
+  const santizedPtrSeq = patternSequence.filter(x => x)
+
+  timestamp
+    .match(new RegExp(regex))
+    .slice(1)
+    .forEach((match, index) => {
+      if (santizedPtrSeq[index].startsWith('hours')) {
+        output.hours = norm(Number(match))
+      }
+      if (santizedPtrSeq[index].startsWith('mins')) {
+        output.minutes = norm(Number(match))
+      }
+      if (santizedPtrSeq[index].startsWith('seconds')) {
+        output.seconds = norm(Number(match))
+      }
+    })
+
+  return output
+}
---
+++ tests/parse.test.mjs
@@ -0,0 +1,34 @@
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+import { parse } from '../src/index.mjs'
+
+test('no pattern', () => {
+  const { hours, minutes, seconds } = parse('01h 24m 21s')
+  assert.equal(hours, 1)
+  assert.equal(minutes, 24)
+  assert.equal(seconds, 21)
+})
+
+test('parse {hh}h:{mm}m', () => {
+  const { hours, minutes, seconds } = parse('01h:02m', '{hh}h:{mm}m')
+
+  assert.equal(hours, 1)
+  assert.equal(minutes, 2)
+  assert.equal(seconds, 0)
+})
+
+test('parse {h}h{m}m', () => {
+  const { hours, minutes, seconds } = parse('1h2m', '{h}h{m}m')
+  assert.equal(hours, 1)
+  assert.equal(minutes, 2)
+  assert.equal(seconds, 0)
+})
+
+test('parse {s}s|{ss}s', () => {
+  const { hours, minutes, seconds } = parse('2s|02s', '{s}s|{ss}s')
+  assert.equal(hours, 0)
+  assert.equal(minutes, 0)
+  assert.equal(seconds, 2)
+})
+
+test.run()
